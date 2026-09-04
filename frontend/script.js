6450f49723e639a026551958eb6e6e2500e9b6ab
Anand Samadhiya <b8582r@gmail.com>
2026-09-04 20:34:10 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,5 @@
-const API_URL = "http://127.0.0.1:8000/predict";
+// ===== CONFIG =====
+const API_URL = "https://diabetes-assistant.onrender.com/predict";
 
 document.getElementById("patient-form").addEventListener("submit", async function(e) {
   e.preventDefault();
